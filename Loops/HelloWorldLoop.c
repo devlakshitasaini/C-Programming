46f3cac249cc81105a3b39e0ee13bf0866dee302
Lakshita Saini <lakshitasaini027@gmail.com>
2026-03-24 21:31:37 +0530

    Loops

---
+++ Loops/HelloWorldLoop.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int number;
+    printf("Enter how many times you want :");
+    scanf("%d",&number);
+    for(int i=1;i<=number;i++)
+    printf("Hello World\n");
+    return 0;
+}
