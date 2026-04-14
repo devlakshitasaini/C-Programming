5dc78c93dc234a8f84806ee83cbdba5ffc70aaa2
Lakshita Saini <lakshitasaini027@gmail.com>
2026-04-14 16:30:30 +0530

    call 2 functions

---
+++ function/function.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void printhello(){
+    printf("hello\n");
+}
+void printgoodbye(){
+    printf("goodbye");
+}
+int main(){
+    printhello();
+    printgoodbye();
+    return 0;
+}
+
+
+
+
